d6948c41ad6ed14119147b94bbc684e86764d1d9
smasa1112 <mtsakagami1112@yahoo.co.jp>
2022-05-04 09:47:07 +0900

    [manage]参考コードの追加

---
+++ NeuroProcessing/Old_Script_for_ref/re.py
@@ -0,0 +1,16 @@
+from email.mime import base
+import re
+
+target_string="us_burst_158V_window_0_prf_1500Hz_pd_160us.mat"
+# 文字列の付与部分をあとから追記するようにするほうがよさそう
+base_info = re.search("us_burst_(\d+V)",target_string)
+volt=base_info.group(1)
+start=base_info.end()
+if len(target_string)!=(start+4):
+    param_info=target_string[start+1:-4]
+    params=list(param_info.split("_"))
+    dict= {}
+    for i in range(len(params)//2):
+        dict[params[2*i]]=params[2*i+1]
+    print(dict)
+print(volt)
