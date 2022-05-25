52838ba5330f95ee2605832c7780ce17a4c35f3e
smasa1112 <mtsakagami1112@yahoo.co.jp>
2022-05-25 11:17:48 +0900

    requirementの追加

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='NeuroProcessing',
-    version='0.0',
+    version='0.1',
     packages=find_packages(),
+    install_requires=[
+        "numpy","pandas","pathlib2","pathlib",
+        "openpyxl","matplotlib"
+    ],
     # MANIFEST.in を使うために必要
-    include_package_data=True
+    include_package_data=True,
+    python_requires=">=3.7"
 )
