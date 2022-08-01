8c7a12424c0d416656a57ff131983dc292cb59ea
smasa1112 <mtsakagami1112@yahoo.co.jp>
2022-08-01 16:45:22 +0900

    add default property to settings

--- NeuroProcessing/Setting.py
+++ NeuroProcessing/Setting.py
@@ -1,4 +1,5 @@
 #modules
+from email.policy import default
 import pandas as pd
 import numpy as np
 import json
@@ -39,45 +40,75 @@ class NumpyEncoder(json.JSONEncoder):
 
 
 class PlotSetting():
-    def __init__(self,json_filename:str = ""):
+    default_data={
+    "xlim" : [-50,350],
+    "ylim" : [-200,200],
+    "channelmap" : [8,9,7,10,4,13,5,12,2,15,1,16,3,14,6,11],
+    "linewidth" : 2,
+    "xlabel" : "time from stimulation[ms]",
+    "ylabel" : "depth[μm]",
+    "title": "Waveplot",
+    "lfp_ylim" : [-200,200],
+    "abr_ylim" : [-1,1],
+    "samplerate": 40000,
+    "csd_vrange" : 30
+    }
+    def __init__(self,json_filename:str = "", default_data:dict = default_data):
         if len(json_filename)==0:
-                json_filename="setting_json/plot_setting.json"
-        with open(json_filename) as setting_file:
-            params=json.load(setting_file)
-            self.xlim: list[float] = params["xlim"]
-            self.ylim: list[float] = params["ylim"]
-            self.channelmap: list[int] =params["channelmap"]
-            self.samplerate: int = params["samplerate"]
-            self.linewidth: float = params["linewidth"]
-            self.xlabel: str= params["xlabel"]
-            self.ylabel: str = params["ylabel"]
-            self.title : str = params["title"]
-            self.lfp_ylim: list[float] = params["lfp_ylim"]
-            self.abr_ylim: list[float] = params["abr_ylim"]
-            self.csd_vrange: int = params["csd_vrange"]
+                for key,value in default_data.items():
+                    setattr(self,key,value)
+        else:            
+            with open(json_filename) as setting_file:
+                params=json.load(setting_file)
+                self.xlim: list[float] = params["xlim"]
+                self.ylim: list[float] = params["ylim"]
+                self.channelmap: list[int] =params["channelmap"]
+                self.samplerate: int = params["samplerate"]
+                self.linewidth: float = params["linewidth"]
+                self.xlabel: str= params["xlabel"]
+                self.ylabel: str = params["ylabel"]
+                self.title : str = params["title"]
+                self.lfp_ylim: list[float] = params["lfp_ylim"]
+                self.abr_ylim: list[float] = params["abr_ylim"]
+                self.csd_vrange: int = params["csd_vrange"]
 
 class WaveSetting:
 # class for waveform
+    default_data={
+    "samplerate":40000,
+    "time_prefix": "ms",
+    "voltage_prefix" : "μV",
+    "timespan": [-50,350]
+    }
 # testing class
     def __init__(self,json_filename:str = ""):
         if len(json_filename)==0:
-            json_filename="setting_json/wave_setting.json"
-        with open(json_filename) as setting_file:
-            params=json.load(setting_file)
-            self.samplerate: int = params["samplerate"]
-            self.time_prefix : str = params["time_prefix"]
-            self.voltage_prefix: str = params["voltage_prefix"]
-            self.timespan : list[int] = params["timespan"]
+                for key,value in WaveSetting.default_data.items():
+                    setattr(self,key,value)
+        else:            
+            with open(json_filename) as setting_file:
+                params=json.load(setting_file)
+                self.samplerate: int = params["samplerate"]
+                self.time_prefix : str = params["time_prefix"]
+                self.voltage_prefix: str = params["voltage_prefix"]
+                self.timespan : list[int] = params["timespan"]
 
 class RecordSetting:
+    default_data = {
+    "event_ch" : "EVT01",
+    "spkc_samplerate" : 40000,
+    "lfp_ts_ch" : "FP01_ts"
+    }
     def __init__(self,json_filename:str = ""):
         if len(json_filename)==0:
-            json_filename="setting_json/record_setting.json"
-        with open(json_filename) as setting_file:
-            params=json.load(setting_file)
-            self.spkc_samplerate: int = params["spkc_samplerate"]
-            self.event_ch : str = params["event_ch"]
-            self.lfp_ts_ch: str = params["lfp_ts_ch"]
+            for key,value in RecordSetting.default_data.items():
+                setattr(self,key,value)
+        else:            
+            with open(json_filename) as setting_file:
+                params=json.load(setting_file)
+                self.spkc_samplerate: int = params["spkc_samplerate"]
+                self.event_ch : str = params["event_ch"]
+                self.lfp_ts_ch: str = params["lfp_ts_ch"]
             
 def get_args(func:Any):
     #function型を使おうとするとerrorになるため一旦引数をAny型に変更
