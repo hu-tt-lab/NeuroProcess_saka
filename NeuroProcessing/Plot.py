d1282ab77e5f82c592f1d6620ee172e7ebebab8c
smasa1112 <mtsakagami1112@yahoo.co.jp>
2022-08-02 13:00:40 +0900

    fix

--- NeuroProcessing/Plot.py
+++ NeuroProcessing/Plot.py
@@ -24,7 +24,7 @@ def plastic_key(key):
         print("vol not found")
         start=0
         vol=""
-    params=re.findall("[a-zA-Z]*_[0-9]+\.*[0-9]*μ*[a-zA-Z]*%*",key[start:])
+    params=re.findall("[a-zA-Z]+_[0-9]+\.*[0-9]*μ*[a-zA-Z]*%*",key[start:])
     params="\n".join(params)
     plasticed_key=f"{vol}_{params}"
     return plasticed_key
