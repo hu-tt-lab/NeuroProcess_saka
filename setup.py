4b5d0c8de17d76fc966c0f899b06dab030eeb02f
smasa1112 <mtsakagami1112@yahoo.co.jp>
2023-02-01 15:07:17 +0900

    fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         "numpy","pandas","pathlib2","pathlib",
-        "openpyxl","matplotlib"
+        "openpyxl","matplotlib","scipy"
     ],
     # MANIFEST.in を使うために必要
     include_package_data=True,
